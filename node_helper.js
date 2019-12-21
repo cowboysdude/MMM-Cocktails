ec92015060abf7b0609fdfa8ddc0e2abd6a9320e
John Wade <johnw8017@gmail.com>
2019-12-20 19:13:50 -0500

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,13 +1,11 @@
 /* Magic Mirror
  * Module: MMM-Cocktails
  *
- * By Mykle1
+ * By Mykle1 ~ edited by Cowboysdude
  *
  */
 const NodeHelper = require('node_helper');
 const request = require('request');
-
-
 
 module.exports = NodeHelper.create({
 
@@ -22,8 +20,27 @@ module.exports = NodeHelper.create({
         }, (error, response, body) => {
             if (!error && response.statusCode == 200) {
                 var result = JSON.parse(body).drinks[0];
+				var recipe = {
+                    recipeName: result.strDrink,
+                    instruction: result.strInstructions,
+                    thumb: result.strDrinkThumb,
+                    glass: result.strGlass,
+                    category: result.strCategory,
+                    ingredients: []
+                };
+				for (var i = 1; i <= 24; i++) {
+                    if (!result['strIngredient' + i]) {
+                        break;
+                    }
+					if (!result['strMeasure' + i]) {
+                        break;
+                    }
+                    recipe.ingredients.push({
+                        ingredient: result['strMeasure' + i] + "" + result['strIngredient' + i]
+                    });
+                }
 			//	console.log(response.statusCode); // for checking
-                this.sendSocketNotification('COCKTAILS_RESULT', result);
+                this.sendSocketNotification('COCKTAILS_RESULT', recipe);
             }
         });
     },
